2e5ef827c391cf7743932b653b613536b5d6dd6b
Richard Hurt <rnhurt@gmail.com>
2025-04-10 07:50:41 -0400

    Remove comments

--- spec/puma_cloudwatch/metrics/fetcher_spec.rb
+++ spec/puma_cloudwatch/metrics/fetcher_spec.rb
@@ -1,19 +1,3 @@
-# RSpec.describe PumaCloudwatch::Metrics::Fetcher do
-#   subject(:fetcher) { described_class.new(control_url: '', control_auth_token: '') }
-
-#   describe "fetcher" do
-#     it "call" do
-#       fake_data = {"fake" => "data"}
-#       json_data = JSON.dump(fake_data)
-#       allow(Socket).to receive(:unix).and_return(json_data)
-
-#       stats = fetcher.call
-#       expect(stats).to eq(fake_data)
-#     end
-#   end
-# end
-
-
 RSpec.describe PumaCloudwatch::Metrics::Fetcher do
   subject(:fetcher) { described_class.new(control_url: 'unix:///tmp/fake.sock', control_auth_token: '') }
 
