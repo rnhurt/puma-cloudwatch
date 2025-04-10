55d6350561c192f90e3ef8e16d1a309d3134fcc2
Richard Hurt <rnhurt@gmail.com>
2025-04-10 07:49:20 -0400

    Fix fetcher specs

--- spec/puma_cloudwatch/metrics/fetcher_spec.rb
+++ spec/puma_cloudwatch/metrics/fetcher_spec.rb
@@ -1,11 +1,32 @@
+# RSpec.describe PumaCloudwatch::Metrics::Fetcher do
+#   subject(:fetcher) { described_class.new(control_url: '', control_auth_token: '') }
+
+#   describe "fetcher" do
+#     it "call" do
+#       fake_data = {"fake" => "data"}
+#       json_data = JSON.dump(fake_data)
+#       allow(Socket).to receive(:unix).and_return(json_data)
+
+#       stats = fetcher.call
+#       expect(stats).to eq(fake_data)
+#     end
+#   end
+# end
+
+
 RSpec.describe PumaCloudwatch::Metrics::Fetcher do
-  subject(:fetcher) { described_class.new(control_url: '', control_auth_token: '') }
+  subject(:fetcher) { described_class.new(control_url: 'unix:///tmp/fake.sock', control_auth_token: '') }
 
   describe "fetcher" do
-    it "call" do
-      fake_data = {"fake" => "data"}
-      json_data = JSON.dump(fake_data)
-      allow(Socket).to receive(:unix).and_return(json_data)
+    it "calls and parses the socket response" do
+      fake_data = { "fake" => "data" }
+      response_body = "HTTP/1.0 200 OK\r\n\r\n#{JSON.dump(fake_data)}"
+
+      mock_socket = double("socket")
+      allow(mock_socket).to receive(:print)
+      allow(mock_socket).to receive(:read).and_return(response_body)
+
+      allow(Socket).to receive(:unix).and_yield(mock_socket)
 
       stats = fetcher.call
       expect(stats).to eq(fake_data)
